a633e3c6b6f8c1a8825712dbf732ada4c131f4bf
Gunny133 <xibianrichu@gmail.com>
2014-11-28 16:10:58 +0800

    [ADD]testDateSort

---
+++ TestDateSort.java
@@ -0,0 +1,58 @@
+public class TestDateSort {
+	 public static void main(String[] args) {
+		Date[] days = new Date[5];
+		days[0] = new Date(2012,5,2);
+		days[1] = new Date(2012,4,2);
+		days[2] = new Date(2012,4,1);
+		days[3] = new Date(2014,1,3);
+		days[4] = new Date(2015,3,1); 
+		bubleSort(days);
+		for(int i = 1;i < 5;i++) {
+			System.out.println(days[i]);
+			}
+	
+		}
+		public static void bubleSort(Date[] a) {
+			for(int i = a.length -1;i >= 0;i--){
+				for(int j = 0;j < i-1;j++){
+						if(a[j].compare(a[j+1])>0) {
+							Date temp = a[j];
+							a[j] = a[j+1];
+							a[j+1] = temp;
+						}
+					} 
+				}
+			
+			}
+		public static void selectSort(Date[] a) {
+			for(;;) {
+				for(;;) {
+					
+					}
+				}
+			}
+	}
+
+class Date { 
+	private int year;
+	private int month;
+	private int day;
+	Date(int y, int m, int d){
+		this.year = y;
+		this.month = m;
+		this.day = d;
+		}
+		
+		public int compare(Date date){
+			return year > date.year ? 1
+							:year < date.year ? -1
+							:month > date.month ? 1
+							:month <date.month ? -1
+							:day > date.day ? 1
+							:day < date.day ? -1
+							: 0;
+			}
+		public String toString() {
+				return "year:month:day--"+year+"-"+month+"-"+day;
+			}
+	}
